ad92463ab370f36aacfa9fce76010b39074139d4
Pau Romero <paurodea@gmail.com>
2024-02-11 18:09:01 +0000

    Created splines module on utils

---
+++ utils/splines.py
@@ -0,0 +1,476 @@
+
+import numpy as np
+
+from scipy.interpolate import (splrep, splev)
+
+
+
+
+
+def knots_list(s0, s1, n, mode='complete', ext=None, k=3):
+    """ Generates a B-Spline uniform list of knots
+
+        Given s0 and s1 it returns a numpy array of nodes for a B-Spline
+
+        Parameters
+        ----------
+
+        s0 : float
+            Starting parameter value
+
+        s1 : float
+            Ending parameter value
+
+        n : int
+            Number of B-Spline knots from s0 to s1.
+
+        ext : int
+            Defaulting to k+1. The times to extend the knot vector from
+            both ends preserving the separation between nodes.
+
+        k : int
+            The degree of the spline.
+
+        mode : {'simple','complete', 'extended', 'periodic'} , optional
+            If mode='simple' then l is a numpy linspace from s0 to s1.
+
+            If mode='complete' l contains [s0,s0,s0] at the beginning and
+            [s1,s1,s1] at the end. Needed for scipy.interpolation.BSpline.
+
+            If mode = 'extended' and ext is not none, it extends ext times the
+            knot vector from both ends preserving the spacing.
+
+            If mode = 'periodic' then the list of knots is ready for periodic
+            B-Splines fitted with splrep. It is the same as setting mode='extended'
+            and ext=k+1.
+
+        Output
+        ------
+            l: the list of knots
+    """
+    l = np.linspace(s0,s1,n+2)
+    d = (s1-s0)/(n+1)
+
+    if mode == 'periodic':
+        mode = 'extended'
+        ext = k+1
+
+    if mode == 'simple':
+        l = l[1:-1]
+    elif mode == 'extended':
+        if ext is None:
+            ext = k+1
+        l = np.concatenate([l[0] + np.arange(-ext, 0)*d, l, l[-1] + np.arange(ext+1)[1:]*d])[1:-1]
+
+    else:
+        initial  = np.array( [s0]*3 )
+        terminal = np.array( [s1]*3 )
+        l = np.concatenate((initial,l,terminal))
+
+    return l
+#
+
+def spline_smoothing(points,
+        param_values=None,
+        n_knots=16,
+        n_fixed_first=1,
+        n_fixed_last=1,
+        fixed_weight=None):
+    """ Smoothing of a list of d-dimensional points
+
+    points must be a numpy array of dimension  Nxd for
+    a list of N d-dimensional points
+
+    TODO: document parameters in spline_smoothing
+    """
+
+    if len(points.shape) == 1:
+        points_data = np.array([points]).T
+    else:
+        points_data = points
+
+    N = points_data.shape[0]
+
+    if param_values is None:
+        param_values = [0]
+        for i in range(1,N):
+            dist_recorrida = np.linalg.norm( points_data[i] - points_data[i-1] )
+            param_values.append(param_values[-1]+dist_recorrida)
+
+
+    #Computing knots
+    knots = knots_list(param_values[0], param_values[-1], n_knots,mode='simple')
+
+    #Computing weights taking into account fixed nodes
+    w = compute_n_weights(n=N, n_fixed_ini=n_fixed_first, n_fixed_end=n_fixed_last, fixed_weight=fixed_weight)
+
+    Lcoefficients = []
+
+    for pts in points_data.T:
+        _,c,_ = splrep(param_values, pts, k=3, w=w, task=-1, t=knots)
+        Lcoefficients.append(c)
+
+    return Lcoefficients
+#
+
+def fix_discontinuity(polar_points, n_first = 10, n_last  = 10, degree = 3, logger=None):
+    """
+    This function expects a 2D point cloud expressed in polar coortinates
+    contained in an array of shape (2,N). This point cloud have to be sorted
+    in theta wise order from 0 to 2pi. If these conditions are fulfilled this method
+    returns a list of points, where the points close to 0 or 2pi have been smoothed by means
+    of a bspline.
+
+    Arguments
+    ----
+    polar_points : numpy.array
+        array of two rows containing the theta and rho coordinates of the point
+        cloud of the form [[theta1, theta2,..., thetaN], [rho1,rho2, ..., rhoN]]
+    n_first : int
+        number of points after the beginning to be used
+    n_last : int
+        number of points before the end to be used
+    degree : int
+        degree of the polynomial to use
+    logger: logging.Logger
+        output logger
+
+    Returns
+    -------
+    cont_polar_points : numpy.array
+        A copy of the array with the discontinuity reduced
+
+    Explanation
+    -----------
+    The last n_last points are placed before the firsr n_first points. Then,
+    a polynomial of degree n is used to approximate a point at theta = 0.
+    This value is added at the beginning and at the end of the vector.
+
+    """
+
+
+    if polar_points.shape[1] < max(n_first,n_last):
+        n_first = min(n_first,polar_points.shape[1])
+        n_last = min(n_last,polar_points.shape[1])
+        if logger is not None:
+            logger.debug(f"Not enough points. Reducing to  n_first = {n_first};  n_last = {n_last}")
+    #
+
+    # Values of theta before and after the cut
+    th_last = polar_points[0,-n_last:] - 2*np.pi
+    th_first = polar_points[0,:n_first]
+
+    # Values of r before and after the cut
+    r_last = polar_points[1,-n_last:]
+    r_first = polar_points[1,:n_first]
+
+    # Vectors with th and r around the cut, in order
+    th = np.concatenate((th_last,th_first))
+    r = np.concatenate((r_last,r_first))
+
+    tck = splrep(th, r, k=degree, s = 0.1)
+
+    cut = splev(0,tck)
+
+    cont_points = np.concatenate(([[0],[cut]],
+                         polar_points,
+                         [[2*np.pi],[cut]]), axis = 1)
+
+    return cont_points
+#
+
+def compute_rho_spline(polar_points, n_knots, k=3, logger=None):
+    """
+    Compute the coefficients of the approximating spline
+    """
+
+    # Adding a value at 0 and 2pi
+    cont_polar_points = fix_discontinuity(polar_points)
+
+    knots = knots_list(0,2*np.pi, n_knots, mode='simple')
+
+    coeff_r, rmse = None, None
+    if polar_points.shape[1] < n_knots + k +1:
+        err_msg = f"compute_slice_coeffs: amount of points ({polar_points.shape[0]}) is less than n_knots_slice + 1 ({n_knots}+1)"
+        if logger is not None:
+            logger.warning(err_msg)
+        else:
+            print(err_msg)
+    else:
+        (_, coeff_r, _), _, ier, msg = splrep(x=cont_polar_points[0],
+                            y=cont_polar_points[1],
+                            k=k,
+                            t=knots,
+                            per=True,
+                            full_output=True)
+        if ier > 0:
+            err_msg = f"splrep failed to fit the slice, saying: ier: {ier}, msg: '{msg}'"
+            if logger is not None:
+                logger.warning(err_msg)
+            else:
+                print(err_msg)
+
+            coeff_r = None
+        else:
+            rmse = compute_slice_rmse(polar_points=polar_points, n_knots_slice = n_knots, coeff=coeff_r)
+
+    return coeff_r, rmse
+#
+
+def compute_slice_coeffs(points, v1, v2, center, n_knots_slice, logger=None, full_output=False, debug=False, k=3):
+    """ Fits a spline to the points of a slice
+
+    TODO: document compute_slice_coeffs
+
+    Returns
+    -------
+    coef_r : numpy.array
+        List of coefficients.
+    min_r : float
+        minimum value of the radius
+    max_r : float
+        maximum value of the radius
+    rmse : float
+        root mean squared error of the fitting
+
+    """
+
+    points2d_cart = coord_planes(points.T, center, v1, v2)
+
+    # Convert to polars. sorted by theta and with 0 <= th < 2pi
+    polar_points = cart_to_polar(points2d_cart)
+
+    coeff_r, rmse = compute_rho_spline(polar_points=polar_points, k=k, n_knots=n_knots_slice, logger=logger)
+
+    max_r = max(polar_points[1])
+    min_r = min(polar_points[1])
+
+    if debug:
+        f = plt.figure()
+        ax_c = f.add_subplot(121)
+        ax_c.set_title('Cartesian coords')
+        ax_c.scatter(points2d_cart[0], points2d_cart[1])
+
+        ax_p = f.add_subplot(122)
+        ax_p.set_title('Polar coords')
+        ax_p.scatter(polar_points[0], polar_points[1])
+
+        plt.show()
+
+
+    # The last four values are always 0. We do not remove them
+    if full_output:
+        return coeff_r, min_r, max_r, rmse
+    else:
+        return coeff_r, min_r, max_r
+#
+
+def compute_slice_rmse(polar_points, coeff, n_knots_slice, k=3):
+
+    error = 0
+    if not isinstance(polar_points, np.ndarray): polar_points = np.array(polar_points)
+
+    t = knots_list(0, 2*np.pi, n_knots_slice, mode='periodic')
+
+    for p in polar_points.T:
+        e = (p[1] - splev(p[0], (t,coeff,k)))**2
+        error += e
+
+    error = np.sqrt(error/polar_points.shape[1])
+    return error
+#
+
+def compute_point_weights(points, weighting=None, normalize=True):
+    """
+    Function to compute weights for a list of points. The weighting of the points
+    is carried by means of the weighting argument. It should be a list of tuples
+    (v, ax, w) specifying the value v, at axis ax, to be weighted by w.
+    If normalize is True (which is the default) The weights are normalized to add
+    up to 1 for optimization's sake.
+
+    Arguments:
+    -----------
+
+        points : np.ndarray, NxD
+            The array of D-dimensional points.
+
+        weighting : array-like, Mx3.
+            The list of tuples with the value, the axis and weights
+            E.g. [(0, 0, 2), (np.pi, 1, 0.5)],
+            with this selection, the points with first coordinate
+            equal to 0 will weight double the base weight, and those
+            whose second coordinate is equal to np.pi will weight half
+            the base weight.
+
+        normalize : bool
+            Default True. Whether to normalize the weights to add up to 1.
+
+    Returns:
+    ---------
+
+        weights : np.ndarray
+            The array of weights
+
+    """
+    weights = np.ones((points.shape[0],))
+
+    if weighting is not None:
+        for v, ax, w in weighting:
+            i = np.isclose(points[:,ax], v, rtol=1e-05, atol=1e-08)
+            weights[i] = w
+
+    if normalize:
+        weights /= weights.sum()
+
+    return weights
+#
+
+def extend_periodically_point_cloud(pts, col=1, T=None, d_max=None):
+    """
+
+    Args:
+    ------
+        pts : np.array Nx3
+            Array of points
+
+        col : int,
+            The periodic variable. By default it is 1, i.e. y axis.
+
+        T : float, optional
+            The period of the function. By default it is pts[:,col].max()-pts[:,col].min()
+
+        d_max : float
+            The maximum distance to extend the period in both sides. By default it is T.
+
+    """
+
+    m = pts[:,col].min()
+    M = pts[:,col].max()
+
+    if T is None:
+        T = M-m
+
+    if d_max is None:
+        d_max = T
+
+    n_copies = int(1 + (T // d_max))
+
+    pts_out = pts.copy()
+    for _ in range(n_copies):
+        L = pts.copy()
+        L[:,col] -= T
+        U = pts.copy()
+        U[:,col] += T
+        pts_out = np.concatenate( (L, pts_out, U))
+    pts_out = pts_out[ (pts_out[:,col] > m - d_max) & (pts_out[:,col] < M + d_max) ]
+
+    return np.unique(pts_out,axis=0)
+#
+
+def semiperiodic_LSQ_bivariate_approximation(x, y, z, nx, ny, weighting=None, ext=None, kx=3, ky=3, debug=False):
+    """
+    A function to perform a LSQ approximation of a bivariate function, f(x,y),
+    that is periodic wrt the y axis, by means of bivariate splines. To emulate periodicity
+    the provide points are periodically extended in the y-axis, and a bivariate spline with
+    its corresponding extended knots is fitted in that space.
+
+    Arguments:
+    ------------
+
+        x,y,z : np.ndarray
+            The samples f(x_i,y_i) = z_i. The three arrays must have the same length.
+
+        weighting : list[tuples], [(v, ax, w)]
+            The list of values v, at axis ax, to be weighted by w. For example, to
+            weight those points with x coordinate equal to 1, with a weight double to
+            the rest, weighting=[(1,0,2)] meaning, the points equals to 1 in the ax 0 weight 2.
+
+        nx,ny : int
+            The number of subdivisions for each dimension.
+
+        ext : int
+            The amount of partitions to add for the periodic extension.
+
+        kx,ky : int
+            The degree of the spline for each dimension.
+
+        degbug : bool
+            Display a plot with the extension of the data and the fitting result.
+
+    Returns:
+    ---------
+        Mcoeff : np.ndarray (nx+kx+1, ny+ky+1)
+            The coefficients of the bivariate spline.
+    """
+
+    if ext is None:
+        ext = ky+1
+
+    xb, xe = x.min(), x.max()
+    yb, ye = y.min(), y.max()
+
+    tx = knots_list(s0=xb, s1=xe, n=nx, mode='simple')
+    ty = knots_list(s0=yb, s1=ye, n=ny, mode='extended', ext=ext-1)
+    d = ty[1] - ty[0]
+
+    pts = np.concatenate((x.reshape(-1,1), y.reshape(-1,1), z.reshape(-1,1)), axis=1)
+    pts_ext = extend_periodically_point_cloud(pts, col=1, T=2*np.pi, d_max=ext*d)
+    x_ext, y_ext, z_ext = pts_ext[:,0], pts_ext[:,1], pts_ext[:,2]
+    yb_ext, ye_ext = y_ext.min(), y_ext.max()
+
+    weights = compute_point_weights(pts_ext, weighting=weighting)
+    if debug:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(x_ext, y_ext, z_ext, c=z_ext, label='ext pts', s=5*(weights/weights.min()), alpha=0.5) #Extended
+        ax.scatter(x, y, z, color='k', s=5, label='orig pts') #Original
+        plt.title('Weights and extension')
+        plt.legend()
+        plt.show()
+
+
+    ty_ext = knots_list(s0=yb, s1=ye, n=ny, mode='extended', ext=ext-1)
+
+    bspl_ext = LSQBivariateSpline(x_ext, y_ext, z_ext, tx=tx, ty=ty_ext, w=weights, bbox=[xb, xe, yb_ext, ye_ext], kx=kx, ky=ky)
+
+    nyy = ny + 2*(ext-1)
+    Mcoeff_ext=bspl_ext.get_coeffs().reshape(nx+kx+1,nyy+ky+1)
+
+    Mcoeff = Mcoeff_ext[:,ext-1:1-ext]
+
+
+    if debug:
+        #Build the spline
+        bspl_rest = BivariateSpline()
+        tyy_rest = knots_list(s0=yb, s1=ye, n=ny, mode='periodic')
+        txx = knots_list(s0=xb, s1=xe, n=nx, mode='complete')
+        bspl_rest.tck = txx, tyy_rest, Mcoeff.ravel()
+        bspl_rest.degrees = kx, ky
+
+        #Make the analytic data.
+        ngrid = 100
+        xx = np.linspace(xb, xe, ngrid+1)
+        yy = np.linspace(yb, ye, ngrid+1)
+        yy_ext = np.linspace(yb_ext, ye_ext, ngrid+1)
+        X_ext, Y_ext = np.meshgrid(xx, yy_ext)
+        X, Y = np.meshgrid(xx, yy)
+        Z_ext = bspl_ext(xx, yy_ext).T
+        Z_rest = bspl_rest(xx,yy).T
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+        #Plot the surfaces.
+        surf = ax.plot_surface(X_ext, Y_ext, Z_ext, color='g', alpha=0.5,
+                            linewidth=0, antialiased=True, label='ext bvspl')
+
+        surf = ax.plot_surface(X, Y, Z_rest, color='r', alpha=0.9,
+                            linewidth=0, antialiased=True, label='rest bvspl')
+
+        #Plot the points
+        ax.scatter(pts_ext[:,0], pts_ext[:,1], pts_ext[:,2], color='b', alpha=0.5, s=2, label='ext pts') #Extended
+        ax.scatter(x, y, z, color='k', label='ext pts') #Original
+        #plt.legend() Due to a bug in matplotlib (3.5.1), this cant be uncomented
+        plt.show()
+
+    return Mcoeff
+#
