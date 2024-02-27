af71ef48659209c9c80078917d7e8cf23b183390
Pau Romero <paurodea@gmail.com>
2024-02-27 17:35:21 +0000

    Highlight commentary with a TODO

--- centerline/centerline.py
+++ centerline/centerline.py
@@ -887,7 +887,7 @@ class Centerline(UniSpline, Node):
             k       = spl.k,
             knots   = spl.t,
             coeffs  = spl.c,
-            n_knots = len(spl.t) - 2*(spl.k+1), #We only account for internal knots This should be explained somewhere....
+            n_knots = len(spl.t) - 2*(spl.k+1), #TODO: We only account for internal knots This should be explained somewhere....
             extra   = 'linear')
 
         cl.compute_adapted_frame(mode=pt_mode, p=p)
