a22346c0c49cb4f39a288081646a12cafa14563a
Pau Romero <paurodea@gmail.com>
2024-03-28 18:34:12 +0000

    Using template directory

--- scripts/config/readers.py
+++ scripts/config/readers.py
@@ -2,7 +2,7 @@
 import json
 from copy import deepcopy
 
-from ..config import _defaults_dir
+from . import _defaults_dir
 
 def read_json(file):
     """
@@ -25,7 +25,7 @@ def read_json(file):
     return params
 #
 
-def get_json_reader(default_name, template_file):
+def get_json_reader(default_name):
     """
     Function factory to get specific parameter readers.
 
@@ -47,7 +47,7 @@ def get_json_reader(default_name, template_file):
             and a template.
     """
 
-    params = read_json(template_file)
+    params = read_json(os.path.join(_defaults_dir, default_name))
     fname = default_name
 
     def json_reader(path=None,abs_path=False):
@@ -56,17 +56,23 @@ def get_json_reader(default_name, template_file):
         try:
             if abs_path:
                 json_file = path
+                path = os.path.dirname(json_file)
             else:
-                json_file = path + "/" + fname
+                json_file = os.path.join(path, fname)
 
             read_params = read_json(json_file)
             for k in read_params:
                     new_params[k] = read_params[k]
-        except (FileNotFoundError,TypeError):
-            print("Could not find {json_file}. Assuming default parameters.")
+        except (FileNotFoundError, TypeError):
+            print(f"Could not find {json_file}. Assuming default parameters.")
 
         return params
+        return new_params
     #
 
     return json_reader
+#
+
+
+read_centerline_config = get_json_reader(default_name="centerline.json")
 #
