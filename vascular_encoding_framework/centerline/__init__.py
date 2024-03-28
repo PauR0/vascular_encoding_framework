20a0c77efcf8e4a21b8a9e6b499b7dbe78b14da4
Pau Romero <paurodea@gmail.com>
2024-03-28 18:46:52 +0000

    exposing new functions on top of centerline module

--- vascular_encoding_framework/centerline/__init__.py
+++ vascular_encoding_framework/centerline/__init__.py
@@ -2,13 +2,15 @@
 __all__ = ['Centerline',
            'CenterlineNetwork',
            'ParallelTransport',
+           'extract_centerline',
            'Seekers',
            'Flux',
            'extract_centerline_domain',
-           'CenterlinePathExtractor']
+           'CenterlinePathExtractor',
+           'extract_centerline_path']
 
 
-from .centerline import Centerline, CenterlineNetwork, ParallelTransport
+from .centerline import Centerline, CenterlineNetwork, ParallelTransport, extract_centerline
 from .domain_extractors import Seekers, Flux, extract_centerline_domain
-from .path_extractor import CenterlinePathExtractor
+from .path_extractor import CenterlinePathExtractor, extract_centerline_path
 #
