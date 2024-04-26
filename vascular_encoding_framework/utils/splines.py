dd030224a4695ee5f19d53c07981b4b818cdab12
Pau Romero <paurodea@gmail.com>
2024-04-26 17:39:14 +0100

    Fixed bug with periodic grid evaluation

--- vascular_encoding_framework/utils/splines.py
+++ vascular_encoding_framework/utils/splines.py
@@ -298,7 +298,11 @@ class BiSpline(Spline):
         """
 
         def clip_periodic(a, T=2*np.pi):
-            return a - (a//T)*T
+            a = a.copy()
+            p = a//T
+            ids = (p < 0) | (1 < p)
+            a[ids] -= T*p[ids]
+            return a
 
         if self.extra_x == 'constant':
             x = np.clip(x, self.x0, self.x1)
