915adaba428d821dc723bbcfbe3907e9d64ec2a0
Pau Romero <paurodea@gmail.com>
2024-05-22 13:22:50 +0200

    Massive refactor on vef_scripts CLI via __main__

--- vef_scripts/vef_scripts/__main__.py
+++ vef_scripts/vef_scripts/__main__.py
@@ -16,9 +16,10 @@ from .vef_make_case import make_case
 from .vef_case import update_ids, show_boundaries
 from .vef_compute_centerline import compute_centerline
 from .vef_encode import encode
+from .vef_align import align_encodings
+from .vef_cohort import cohort_run, _Updater, _Centerliner, _Encoder
 
-
-MODES = {'make_case', 'run_case', 'GPA', 'SSM'}
+MODES = {'case', 'cohort'}
 
 
 parser = argparse.ArgumentParser(description="""Commandline interface of the vascular encoding
@@ -33,27 +34,38 @@ parser.add_argument('-f',
                     '--force',
                     dest='force',
                     action='store_true',
-                    help="""Force the recomputation of intermediate steps of centerline.""")
+                    help="""Force the recomputation of intermediate steps of centerline computation.""")
 
 parser.add_argument('-w',
                     action='store_true',
-                    help="""Force overwriting if it already exists.""")
+                    help="""Overwrite existing files.""")
 
-parser.add_argument('-cp',
+parser.add_argument('--cp',
                     '--cl-params',
                     '--centerline-params',
                     dest='cl_params',
                     type=str,
                     default=None,
-                    help = """Alternative json file with the desired centerline params.""")
+                    help = """Path to an alternative json file with the desired centerline params.""")
 
-parser.add_argument('-ep',
+parser.add_argument('--ep',
                     '--ec-params',
                     '--encoding-params',
                     dest='ec_params',
                     type=str,
                     default=None,
-                    help = """Alternative json file with the desired encoding params.""")
+                    help = """Path to an alternative json file with the desired encoding params.""")
+
+parser.add_argument('--gpa-params',
+                    dest='gpa_params',
+                    type=str,
+                    default=None,
+                    help = """Path to an alternative json file with the desired alignment parameters.""")
+
+parser.add_argument('--align',
+                    dest="align",
+                    action="store_true",
+                    help="""For cohort mode only. Run the GPA algorithm on the encodings of a cohort.""")
 
 parser.add_argument('--update-ids',
                     dest="upids",
@@ -62,12 +74,14 @@ parser.add_argument('--update-ids',
                     action='store',
                     const=True,
                     help="""Path to a json file with the ids to be changed with format {"old_id":"new_id"}.
-                    If no argument is provided, the file new_ids.json will be loaded from case dir.""")
+                    If no argument is provided, the file new_ids.json will be loaded from case dir. WARNING:
+                    If an argument is provided in cohort mode, all the cases will update the boundaries with
+                    the provided json file.""")
 
 parser.add_argument('--ascii',
                     dest='binary',
                     action='store_false',
-                    help="""Whether to write ouput files in binary. Default is binary mode.""")
+                    help="""Whether to write ouput files in ascii. Default is binary mode.""")
 
 parser.add_argument('--show-boundaries',
                     dest="show_boundaries",
@@ -75,37 +89,65 @@ parser.add_argument('--show-boundaries',
                     help="""Display the input mesh and boundaries stored on Meshes dir.""")
 
 parser.add_argument('-e',
-                    '-encode',
+                    '--encode',
                     dest='encode',
                     action='store_true',
-                    help="""Ignored if mode is not run_case. Run vascular encoding. Requires
-                    mesh_input, boundaries_input and centerline to exist""")
+                    help="""Run vascular encoding. Requires mesh_input, boundaries_input and
+                    centerline to exist at case directory.""")
 
 parser.add_argument('-c',
-                    '-centerline',
+                    '--centerline',
                     dest='centerline',
                     action='store_true',
-                    help="""Ignored if mode is not run_case. Run vascular centerline computation.
-                    Requires mesh_input, boundaries_input to exist and to have a propper""")
+                    help="""Run vascular centerline computation. Requires mesh_input, boundaries_input
+                    to exist and to have a propper hierarchy.""")
+
+parser.add_argument('--make',
+                    dest='make',
+                    action='store_true',
+                    help="""Make the case directory using the arugments provided.""")
 
 parser.add_argument('--mesh',
                     dest='mesh',
                     type=str,
                     default=None,
-                    help = """Ignored if mode is not make_case. Path to an existing mesh file.""")
+                    help = """Ignored if --make is not raised. Path to an existing mesh file to
+                    set as input mesh..""")
 
-parser.add_argument('--case',
-                    dest='case',
+parser.add_argument('--exclude',
+                    dest='exclude',
+                    type=str,
+                    nargs='+',
+                    default=None,
+                    help = """Ignored if mode is not cohort. List of directories at cohort directory
+                    to be excluded.""")
+
+parser.add_argument('--n-proc',
+                    dest='n_proc',
+                    type=int,
+                    default=1,
+                    help = """Default is 1. Number of parallel procesess to use in cohort mode routines.""")
+
+parser.add_argument('--path',
+                    dest='path',
                     action='store',
                     default=None,
-                    help="""The path/name with the case to be created/run. In make_case mode, if
-                    not provided, a directory called vef_case is created at current working
-                    directory. In run_case mode, if not provided, current working directory is used.""")
+                    help="""If mode is case:\n\t A path/name with the case to be created/run can be
+                    provided. If --make is raised and --case is not provided, the name vef_case is
+                    used to create a case directory at current working directory. If --make is not
+                    raised and --case is not passed, the current working directory is assumed to be
+                    a case directory and it is used to run the routines.\n
+                    If mode is cohort:\n\t A path to a cohort directory can be passed. If it not
+                    raised in cohort mode, the current working directory will be assumed to be a
+                    cohort directory and used to run the routines.""")
 
 parser.add_argument('mode',
                     action='store',
                     default=None,
-                    help=f"""The mode to run. It must be in [{MODES}].""")
+                    help=f"""The mode to run. It must be in [{MODES}]. If mode is case, the command
+                    order is: make -> upadte_ids -> show_boundaries -> centerline comp -> encoding.
+                    If mode is cohort, then the order is: upadte_ids -> centerline comp -> encoding
+                     -> alignment -> SSM""")
 
 args = parser.parse_args()
 
@@ -123,14 +165,13 @@ if args.ec_params is not None:
     ec_params = read_encoding_config(path=args.ec_params, abs_path=True)
 
 
-if args.mode == 'make_case':
-    make_case(case_dir=args.case, mesh_fname=args.mesh, show_boundaries=args.show_boundaries, overwrite=args.w, cl_params=cl_params, ec_params=ec_params)
-    sys.exit(0)
-#
+if args.mode == 'case':
 
-if args.mode == 'run_case':
+    if args.make:
+        case = make_case(case_dir=args.path, mesh_fname=args.mesh, overwrite=args.w, cl_params=cl_params, ec_params=ec_params)
 
-    case = args.case if args.case is not None else os.getcwd()
+    else:
+        case = args.path if args.path is not None else os.getcwd()
 
     if args.upids:
         update_ids(case_dir=case, new_ids=args.upids)
@@ -147,7 +188,38 @@ if args.mode == 'run_case':
     sys.exit(0)
 #
 
-if args.mode in ['GPA', 'SSM']:
-    print(f"Sorry mode {args.mode} is not implemented yet...")
+if args.mode == 'cohort':
+
+    cohort_dir = args.path if args.path is not None else os.getcwd()
+
+    if args.upids:
+
+        cohort_run(cohort_dir=cohort_dir,
+                   routine=_Updater(new_ids=args.upids),
+                   exclude=args.exclude,
+                   n_proc=args.n_proc)
+
+    if args.centerline:
+
+        cohort_run(cohort_dir=cohort_dir,
+                   routine=_Centerliner(params=cl_params,
+                                        binary=args.binary,
+                                        overwrite=args.w,
+                                        force=args.force,
+                                        debug=False),
+                   exclude=args.exclude,
+                   n_proc=args.n_proc)
+
+    if args.encode:
+
+        cohort_run(cohort_dir=cohort_dir,
+                   routine=_Encoder(params=ec_params,
+                                    binary=args.binary,
+                                    overwrite=args.w,
+                                    debug=False),
+                   exclude=args.exclude,
+                   n_proc=args.n_proc)
+
+
     sys.exit(0)
 #
